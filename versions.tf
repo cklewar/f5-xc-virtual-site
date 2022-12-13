fc257fe2272d894dafe09dd2b5d739315fc76873
cklewar <cklewar@f5.com>
2022-12-13 01:23:44 +0100

    0.11.16 v3

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2.5"
+  required_version = ">= 1.3.0"
 
   required_providers {
     f5xc = {
       source  = "volterraedge/volterra"
-      version = ">= 0.11.11"
+      version = "= 0.11.16"
     }
 
     local = ">= 2.2.3"
