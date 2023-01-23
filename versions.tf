04ca0e397c1bfa6fd41bdcbb369897b445c3f699
cklewar <cklewar@t-online.de>
2023-01-23 18:12:07 +0100

    0.11.18

--- versions.tf
+++ versions.tf
@@ -8,14 +8,13 @@ terraform {
       name = "f5-xc-virtual-site-module"
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = "= 0.11.16"
+      source = "volterraedge/volterra"
+      version = "= 0.11.18"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
